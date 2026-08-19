b29a2480d36ad2735783554c9bb371f1c2bd039a
Easwari <easwarimuruganandham@gmail.com>
2026-08-19 10:38:19 +0530

    Create 355-DesignTwitter

---
+++ 355-DesignTwitter/355-DesignTwitter.java
@@ -0,0 +1,52 @@
+// Last updated: 8/19/2026, 10:39:23 AM
+class Twitter {
+
+    private Map<Integer, Set<Integer>> userMap; 
+    private List<Pair<Integer, Integer>> tweetList;
+
+    public Twitter() {
+        userMap = new HashMap<>();
+        tweetList = new ArrayList<>();
+    }
+    
+    public void postTweet(int userId, int tweetId) {
+
+        userMap.computeIfAbsent(userId, k -> new HashSet<>());
+        tweetList.add(new Pair(userId, tweetId));
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        List<Integer> list = new ArrayList<>();
+        if(!userMap.containsKey(userId)){
+            return list;
+        }
+
+        int index = tweetList.size() - 1;
+
+        while(list.size() < 10 && index >= 0) {
+            Pair<Integer, Integer> pair = tweetList.get(index--);
+            if(pair.getKey() == userId || userMap.get(userId).contains(pair.getKey())) {
+                list.add(pair.getValue());
+            }
+        }
+        
+        return list;
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        userMap.computeIfAbsent(followerId, k -> new HashSet<>()).add(followeeId);
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        userMap.get(followerId).remove(followeeId);
+    }
+}
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter obj = new Twitter();
+ * obj.postTweet(userId,tweetId);
+ * List<Integer> param_2 = obj.getNewsFeed(userId);
+ * obj.follow(followerId,followeeId);
+ * obj.unfollow(followerId,followeeId);
+ */
