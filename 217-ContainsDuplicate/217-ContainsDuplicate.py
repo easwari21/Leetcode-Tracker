655b213513923272235827ce168206dd4574031c
Easwari <easwarimuruganandham@gmail.com>
2026-08-19 10:38:50 +0530

    Create 217-ContainsDuplicate

---
+++ 217-ContainsDuplicate/217-ContainsDuplicate.py
@@ -0,0 +1,9 @@
+# Last updated: 8/19/2026, 10:39:55 AM
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
