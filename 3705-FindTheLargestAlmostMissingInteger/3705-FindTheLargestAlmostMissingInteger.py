1683d4c5f2c8c36382cb8a023197534fc9c773e5
Easwari <easwarimuruganandham@gmail.com>
2026-08-19 10:34:31 +0530

    Create 3705-FindTheLargestAlmostMissingInteger

---
+++ 3705-FindTheLargestAlmostMissingInteger/3705-FindTheLargestAlmostMissingInteger.py
@@ -0,0 +1,20 @@
+# Last updated: 8/19/2026, 10:35:35 AM
+class Solution:
+    def largestInteger(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        freq = Counter(nums)
+
+        if k == 1:
+            candidates = [x for x in freq if freq[x] == 1]
+            return max(candidates) if candidates else -1
+
+        if k == n:
+            return max(nums)
+
+        candidates = []
+        if freq[nums[0]] == 1:
+            candidates.append(nums[0])
+        if freq[nums[-1]] == 1:
+            candidates.append(nums[-1])
+
+        return max(candidates) if candidates else -1
