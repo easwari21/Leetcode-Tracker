863dbf4546c3f16e7a96b84158d46b9f06b70b15
Easwari <easwarimuruganandham@gmail.com>
2026-08-19 10:40:17 +0530

    Create 53-MaximumSubarray

---
+++ 53-MaximumSubarray/53-MaximumSubarray.py
@@ -0,0 +1,10 @@
+# Last updated: 8/19/2026, 10:41:22 AM
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_current = max_global = nums[0]
+        for num in nums[1:]:
+            max_current = max(num, max_current + num)
+            max_global = max(max_global, max_current)
+        return max_global
+
+
