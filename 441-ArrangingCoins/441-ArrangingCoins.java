ee25fbd4b2b52aec9d61d8fe95f6252cc051dfb6
Easwari <easwarimuruganandham@gmail.com>
2026-08-19 10:37:48 +0530

    Create 441-ArrangingCoins

---
+++ 441-ArrangingCoins/441-ArrangingCoins.java
@@ -0,0 +1,15 @@
+// Last updated: 8/19/2026, 10:38:52 AM
+class Solution {
+    public int arrangeCoins(int n) {
+        long l=0,r=n;
+        while(l<=r)
+        {
+            long m=l+(r-l)/2;
+            long k=m*(m+1)/2;
+            if(k==n) return (int)m;
+            else if(k<n) l=m+1;
+            else r=m-1;
+        }
+        return (int)r;
+    }
+}
