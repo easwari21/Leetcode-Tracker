bcc9ede930b6c9e82fa9ba3f699d95106b26e61b
Easwari <easwarimuruganandham@gmail.com>
2026-08-19 10:39:45 +0530

    Create 94-BinaryTreeInorderTraversal

---
+++ 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.java
@@ -0,0 +1,18 @@
+// Last updated: 8/19/2026, 10:40:49 AM
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+
+        inorder(root, res);
+        return res;        
+    }
+
+    private void inorder(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left, res);
+        res.add(node.val);
+        inorder(node.right, res);
+    }    
+}
