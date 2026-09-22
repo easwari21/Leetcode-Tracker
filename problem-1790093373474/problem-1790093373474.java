fa1f85ebb48cd648db2206cff931ca90eb680584
Easwari <easwarimuruganandham@gmail.com>
2026-09-22 21:38:20 +0530

    Create

---
+++ problem-1790093373474/problem-1790093373474.java
@@ -0,0 +1,17 @@
+// Last updated: 9/22/2026, 9:39:33 PM
+1class Solution {
+2    public void reverseString(char[] s) {
+3        int left = 0;
+4        int right = s.length - 1;
+5
+6        while(left < right){
+7            char temp = s[left];
+8            s[left] = s[right];
+9            s[right] = temp;
+10
+11            left++;
+12            right--;
+13        }
+14       
+15    }
+16}
