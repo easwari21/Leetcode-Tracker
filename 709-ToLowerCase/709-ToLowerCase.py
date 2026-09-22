3e9d8e8a3a05af6d7ea477b0dbb469340d3a522f
Easwari <easwarimuruganandham@gmail.com>
2026-09-22 21:40:24 +0530

    Create 709-ToLowerCase

---
+++ 709-ToLowerCase/709-ToLowerCase.py
@@ -0,0 +1,11 @@
+# Last updated: 9/22/2026, 9:41:38 PM
+1class Solution(object):
+2    def toLowerCase(self, s):
+3        lower_case_str = ""
+4        for char in s:
+5            ascii_val = ord(char)
+6            if 65 <= ascii_val <= 90: 
+7                lower_case_str += chr(ascii_val + 32)
+8            else:
+9                lower_case_str += char
+10        return lower_case_str
